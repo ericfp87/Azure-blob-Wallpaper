50e016b2c2c2579de48aa21e7410509128e95aac
Eric Ferreira <eric_fpereira@hotmail.com>
2023-12-26 15:45:40 -0300

    Update wallpaper - enviar azure blob.py

--- wallpaper - enviar azure blob.py
+++ wallpaper - enviar azure blob.py
@@ -1,7 +1,7 @@
 from azure.storage.blob import BlobClient
 import os
 
-cadeia_conexao = 'DefaultEndpointsProtocol=https;AccountName=bulbeenergia;AccountKey=EDCC5GAqNRf32VKfViuLP2ySXjlodeY3zWMFUMk5RJdFqz58KUqT+bAibtknl+xFZ2bVbTCuopS9+AStlDBqMA==;EndpointSuffix=core.windows.net'
+cadeia_conexao = 'cadeia_de_conexao'
 
 for arquivo in os.listdir("G:\\Drives compartilhados\\Bulbers\\Recursos Humanos\\Wallpaper"):
     novo_nome_arquivo_blob = 'RH/wallpaper/fundo-tela-note.png'
@@ -21,7 +21,7 @@ for arquivo in os.listdir("G:\\Drives compartilhados\\Bulbers\\Recursos Humanos\
 
 
 
-# cadeia_conexao = 'DefaultEndpointsProtocol=https;AccountName=bulbeenergia;AccountKey=EDCC5GAqNRf32VKfViuLP2ySXjlodeY3zWMFUMk5RJdFqz58KUqT+bAibtknl+xFZ2bVbTCuopS9+AStlDBqMA==;EndpointSuffix=core.windows.net'
+# cadeia_conexao = 'cadeia_de_conexao'
 
 # for arquivo in os.listdir("G:\\Drives compartilhados\\RH - DP\\01 - RH 2022\\35 - Outros\\9. Wallpaper"):
 #     arquivo_blob = 'RH/wallpaper/' + arquivo
